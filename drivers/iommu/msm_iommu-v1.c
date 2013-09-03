80fadad52a42625d253be65ca58a2b5bc2b2aeb2
Laura Abbott <lauraa@codeaurora.org>
2013-09-03 09:46:03 -0700

    iommu: msm: Program all M2V tables at once
    Currently, M2V tables are programmed per context bank when
    attaching. Attaching to a single context bank will take the
    SMMU out of pass through mode. This means that when one context
    bank is attached, none of the other banks will have M2Vs
    programmed until they are attached. This presents a problem if a
    single context bank is expected to work in pass through mode.
    As a solution, program the M2V table for all context bank when
    the first context bank of an IOMMU is attached. Clear out the
    M2V tables only when the last context is detached.
    
    Change-Id: I9e63000100da88da9992eaf80c6ff958dad0b475
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    
    Conflicts:
    	drivers/iommu/msm_iommu-v1.c

--- drivers/iommu/msm_iommu-v1.c
+++ drivers/iommu/msm_iommu-v1.c
@@ -819,6 +819,11 @@ static void msm_iommu_detach_dev(struct iommu_domain *domain,
 	ctx_drvdata->asid = -1;
 
 	__reset_context(iommu_drvdata->base, ctx_drvdata->num);
+	
+	/*
+	 * Only reset the M2V tables on the very last detach */
+	if (!is_secure && iommu_drvdata->ctx_attach_count == 1)
+		__release_smg(iommu_drvdata->base);
 
 	/*
 	 * Only reset the M2V tables on the very last detach */
