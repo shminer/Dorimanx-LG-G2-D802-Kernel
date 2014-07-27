fccd6f355ed7d266311e3784ada87414c230d900
Dorimanx <yuri@bynet.co.il>
2014-07-27 15:59:07 +0300

    MMC: Fix missing function, it's no longer exist long time ago.
    This is just cleanup.

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2709,10 +2709,8 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 	unsigned long flags;
 
 #ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
-	if (mmc_bus_needs_resume(card->host)) {
+	if (mmc_bus_needs_resume(card->host))
 		mmc_resume_bus(card->host);
-		mmc_blk_set_blksize(md, card);
-	}
 #endif
 
 	if (req && !mq->mqrq_prev->req) {
