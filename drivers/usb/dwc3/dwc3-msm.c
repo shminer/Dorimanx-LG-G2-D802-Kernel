b5c32f33dc8bb42ef0fe5f791be349f62e053121
shminer <332574643@qq.com>
2015-01-19 09:31:38 +0800

    Revert "usb: dwc3-msm: handle USB_DCP as AC type power supply"
    This reverts commit 049bb2d25b2a11fef42e88d862f3265771131e3d.

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2805,7 +2805,7 @@ dwc3_msm_property_is_writeable(struct power_supply *psy,
 
 
 static char *dwc3_msm_pm_power_supplied_to[] = {
-	"ac",
+	"battery",
 };
 
 static enum power_supply_property dwc3_msm_pm_power_props_usb[] = {
